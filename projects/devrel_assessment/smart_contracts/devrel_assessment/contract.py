4adf4b11b3b931ed27299ea2faeae5f59ecf05a6
Orest gaboda <orest.gaboda@gmail.com>
2024-11-01 00:27:00 +0000

    Initial and main commit

--- projects/devrel_assessment/smart_contracts/devrel_assessment/contract.py
+++ projects/devrel_assessment/smart_contracts/devrel_assessment/contract.py
@@ -1,8 +1,24 @@
-from algopy import ARC4Contract, String
+from algopy import ARC4Contract, Box, Bytes, Global, String, gtxn
 from algopy.arc4 import abimethod
 
 
 class DevrelAssessment(ARC4Contract):
+    def __init__(self) -> None:
+        self.box = Box(Bytes, key=b"B")
+
+    @abimethod()
+    def fund_account(self, payment: gtxn.PaymentTransaction) -> None:
+        assert (
+            payment.receiver == Global.current_application_address
+        ), "Payment receiver must be the application address"
+
+  
     @abimethod()
     def hello(self, name: String) -> String:
-        return "Hello, " + name
+        greetings = "Hello, " + name
+        self.box_put(response=greetings)
+        return greetings
+    
+    @abimethod()
+    def box_put(self, response: String) -> None:
+        self.box.value = response.bytes
--- projects/devrel_assessment/smart_contracts/devrel_assessment/deploy_config.py
+++ projects/devrel_assessment/smart_contracts/devrel_assessment/deploy_config.py
@@ -1,8 +1,18 @@
+import base64
 import logging
 
 import algokit_utils
 from algosdk.v2client.algod import AlgodClient
 from algosdk.v2client.indexer import IndexerClient
+from algosdk.transaction import PaymentTxn
+from algokit_utils import (
+    TransactionParameters
+)
+from algosdk.atomic_transaction_composer import (
+    AccountTransactionSigner,
+    TransactionWithSigner,
+)
+
 
 logger = logging.getLogger(__name__)
 
@@ -28,9 +38,42 @@ def deploy(
         on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
         on_update=algokit_utils.OnUpdate.AppendApp,
     )
-    name = "world"
-    response = app_client.hello(name=name)
+    # Box name, could be replaced with account.address
+    val = b"B"
+    # App account topup amount in microAlgo
+    amt = 1000_000
+
+    # Construct payment transaction to fund the app account
+    ptxn = PaymentTxn(
+        sender=deployer.address,
+        sp=algod_client.suggested_params(),
+        receiver=app_client.app_address,
+        amt=amt,
+    )
+
+    # Fund the app account
+    app_client.fund_account(
+        payment=TransactionWithSigner(ptxn, AccountTransactionSigner(deployer.private_key)),
+        transaction_parameters=TransactionParameters(boxes=[(app_client.app_id, val)]),
+    )
+   
+    # Define dev name
+    name = "Orest Gaboda"
+
+    # Concat greetings
+    response = app_client.hello(name=name,
+        transaction_parameters=algokit_utils.TransactionParameters(
+        boxes=[(app_client.app_id, val)] 
+    ),)
+
+    # Store the response in the box
+    app_client.box_put(response=response.return_value,
+        transaction_parameters=algokit_utils.TransactionParameters(
+        boxes=[(app_client.app_id, val)])
+    )
+
     logger.info(
         f"Called hello on {app_spec.contract.name} ({app_client.app_id}) "
-        f"with name={name}, received: {response.return_value}"
+        f"with name={name}, received: {response.return_value} and put it in a Box with name {val.decode('utf-8')}. " 
+        f"App account was funded for {amt} microAlgos from deployer account"
     )
